6d4925c8b2a124ceb71dde5fba236d0d14cf218c
Veljko <Veljko>
2017-06-06 14:07:25 +0200

    Više primera za interfejs‚

--- ts/dist/class.js
+++ ts/dist/class.js
@@ -64,3 +64,50 @@ var ss = new Student("Pera", "Peric", "1124");
 ss.ime = "Petar";
 ss.indeks = "11549";
 console.log(ss.zdravo());
+var Point = (function () {
+    function Point() {
+    }
+    return Point;
+}());
+var Point2 = (function () {
+    function Point2() {
+    }
+    return Point2;
+}());
+var p = { x: 10, y: 50 };
+//let p2:Point2 = {x: 10, y:50};//Greska!
+var GeometrijskaFigura = (function () {
+    function GeometrijskaFigura(i) {
+        this.ime = i;
+    }
+    return GeometrijskaFigura;
+}());
+var Kvadrat = (function (_super) {
+    __extends(Kvadrat, _super);
+    function Kvadrat(stranica) {
+        var _this = _super.call(this, "Kvadrat") || this;
+        _this.a = stranica;
+        return _this;
+    }
+    Kvadrat.prototype.getPovrsina = function () {
+        return this.a * this.a;
+    };
+    Kvadrat.prototype.getObim = function () {
+        return this.a * 4;
+    };
+    return Kvadrat;
+}(GeometrijskaFigura));
+var gf = new Kvadrat(6);
+console.log("Ova geometrijska figura je " + gf.ime + "\n  sa povr\u0161inom od " + gf.getPovrsina() + " i obimom od " + gf.getObim());
+var Zaposlen = (function () {
+    function Zaposlen(ime, prezime) {
+        this.ime = ime;
+        this.prezime = prezime;
+    }
+    Zaposlen.prototype.sayHello = function () {
+        return "Hello, " + this.ime + " " + this.prezime + "!";
+    };
+    return Zaposlen;
+}());
+var zz = new Zaposlen("Pera", "Peric");
+console.log(zz.sayHello());
--- ts/src/interface.ts
+++ ts/src/interface.ts
@@ -7,3 +7,37 @@ function printLabel(labelledObj: LabelledValue) {
 }
 let myObj:any = {size: 10, label: "Size 10 Object"};
 printLabel(myObj);
+
+interface WineSpecification{
+  type?:string;
+  grape?:string;
+  vintage?:string;
+}
+
+let order1:WineSpecification = {};
+let order2:WineSpecification = {type: "red"};
+let order3:WineSpecification = {type: "red",
+grape: "Tannat", vintage: "2006"};
+
+function orderWine(w:WineSpecification):void{
+  console.log(`We have ordered a nice bottle of ${w.type ? w.type : ""} ${(w.grape) ? w.grape : ""} wine ${w.vintage ? "from " : ""} ${w.vintage ? w.vintage  : ""}`);
+}
+
+orderWine(order1);
+orderWine(order2);
+orderWine(order3);
+
+orderWine({type: "red"});
+//orderWine({type: "red", flavor: "Delicious"});//ERROR
+orderWine({type: "red", flavor: "Delicious"} as WineSpecification);//Works
+
+interface Order{
+  table?:string;
+  [index:number] : WineSpecification;
+}
+
+let order4:Order = {table:"Table 14", 0:{}, 1:{type: "red"}};
+let order5:Order = [{}, {type: "red"}, {type: "red", grape: "Tannat", vintage: "2006"}];
+
+console.log(order4[0]);
+console.log(order5[2]);
