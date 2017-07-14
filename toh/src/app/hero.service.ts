d0aa6b068f79848d2aefb5c022ea0f772a3a67bb
Vladimir Dimitrieski <dimitrieski89@gmail.com>
2017-07-15 00:22:35 +0200

    fixed all the necessary things (headers, cors, etc.)

--- toh/src/app/hero.service.ts
+++ toh/src/app/hero.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Headers, Http} from '@angular/http';
+import {Headers, RequestOptions, Http} from '@angular/http';
 import {Hero} from './hero';
 
 import 'rxjs/add/operator/toPromise';
@@ -7,20 +7,19 @@ import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class HeroService {
-    private heroesUrl = 'http://localhost:8090/heroes/';
-    private heroUrl = 'http://localhost:8090/hero';
+    private heroUrl = 'http://localhost:8090/api/v1/hero';
 
     constructor(private http: Http) {
     }
 
     getHeroes(): Promise<Hero[]> {
-        return this.http.get(this.heroesUrl).toPromise()
-            .then(response => response.json().Data as Hero[])
+        return this.http.get(this.heroUrl).toPromise()
+            .then((response) => response.json() as Hero[])
             .catch(this.handleError);
     }
 
     private handleError(error: any): Promise<any> {
-        console.log('An error occured', error);
+        console.log('An error occurred', error);
         return Promise.reject(error.message || error);
     }
 
@@ -28,24 +27,28 @@ export class HeroService {
         const url = `${this.heroUrl}/${id}`;
         return this.http.get(url)
             .toPromise()
-            .then(response => response.json().Data as Hero)
+            .then(response => response.json() as Hero)
             .catch(this.handleError);
     }
 
     update(hero: Hero): Promise<Hero> {
+        let headers = new Headers({'Content-Type': 'application/json'});
+        let options = new RequestOptions({headers: headers});
         const url = `${this.heroUrl}/${hero.Id}`;
         return this.http
-            .put(url, JSON.stringify(hero))
+            .put(url, JSON.stringify(hero), options)
             .toPromise()
             .then(() => hero)
             .catch(this.handleError);
     }
 
     create(name: string): Promise<Hero> {
+        let headers = new Headers({'Content-Type': 'application/json'});
+        let options = new RequestOptions({headers: headers});
         return this.http
-            .post(this.heroesUrl, JSON.stringify({Id: null, Name: name}))
+            .post(this.heroUrl, JSON.stringify({Id: null, Name: name}), options)
             .toPromise()
-            .then(res => res.json().Data as Hero)
+            .then(res => res.json() as Hero)
             .catch(this.handleError);
     }
 
